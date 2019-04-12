22b94f5c7148b8a03246f4df138e19112fed97b9
Rk-niit <43521152+Rk-niit@users.noreply.github.com>
2019-04-12 13:30:25 +0530

    hi

---
+++ test.py
@@ -0,0 +1,8 @@
+
+t=int(input("enter the test cases"))
+for i in range(t):
+	arr = []
+	elem = int(input("insert how many elements you want:"))
+	for i in range(0, elem):
+    	arr.append(int(input("Enter next no :")))
+	print (arr[3])
